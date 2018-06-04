bbd4076043eaf153de3c8d102b438f466d01243f
levimateus <levimateus2011@gmail.com>
2018-06-04 18:40:41 -0300

    Aula 2 - bootstrap e template

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<h1>Bem vindo ao sistema de classificados</h1>
+<?php require 'pages/header.php'; ?>
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h2>Nós temos hoje 999 anúncios</h2>
+			<p>E mais de 999 usuários cadastrados.</p>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-4">
+				<h4>Pesquisa Avançada</h4>
+			</div>
+			<div class="col-sm-8">
+				<h4>Últimos Anúncios</h4>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+<?php require 'pages/footer.php'; ?>
