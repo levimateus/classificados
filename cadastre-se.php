21018b65446cd695fe1b0c20bbfe2493540b3bb3
levimateus <levimateus2011@gmail.com>
2018-06-06 18:58:16 -0300

    Aula 4: sistema de login

--- cadastre-se.php
+++ cadastre-se.php
@@ -1,3 +1,4 @@
+<?php require 'config.php'; ?>
 <?php require 'pages/header.php'; ?>
 
 <div class="container">
@@ -12,7 +13,20 @@
 			$telefone = addslashes($_POST['telefone']);
 
 			if (!empty($_POST['nome']) && !empty($_POST['senha']) && !empty($_POST['senha'])) {
-				$u->cadastrar($nome, $email, $senha, $telefone);
+				if($u->cadastrar($nome, $email, $senha, $telefone)){
+					?>
+					<div class="alert alert-success">
+						<strong>Parabéns</strong> Cadastrado com sucesso <a href="login.php" class="alert-link">Faça o login agora</a>
+					</div>
+					<?php
+				} else {
+					?>
+					<div class="alert alert-success">
+						Este usuário já existe <a href="login.php" class="alert-link">
+						Faça o login agora</a>
+					</div>
+					<?php
+				}
 			} else {
 				?>
 				<div class="alert alert-warning">
--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php 
 session_start();
 
+global $pdo;
 try {
 	$pdo = new PDO("mysql:dbname=classificados;hostname=localhost", "root", "password");
 } catch (PDOException $e) {
 	echo "FALHOU".$e->getMessage();
+	exit();
 }
  ?>
