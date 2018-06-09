3fbf958b345ddd0d5e552b99f4ff75891ef66009
levimateus <levimateus2011@gmail.com>
2018-06-09 19:45:45 -0300

    Aula 6 - Criação de anúncios

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -8,7 +8,7 @@ class Anuncios
 		$sql = $pdo->prepare("SELECT *, 
 							   (SELECT anuncios_imagens.url 
 								FROM anuncios_imagens 
-								WHERE anuncios_imagens.id_anuncio = anuncio.id 
+								WHERE anuncios_imagens.id_anuncio = anuncios.id 
 								LIMIT 1) 
 								AS url 
 							FROM anuncios 
@@ -21,5 +21,18 @@ class Anuncios
 		}
 
 		return $array;
+	}
+
+	public function addAnuncio($titulo, $categoria, $valor, $descricao, $estado){
+		global $pdo;
+
+		$sql = $pdo->prepare("INSERT INTO anuncios SET titulo = :titulo, id_categoria = :id_categoria, id_usuario = :id_usuario, descricao = :descricao, valor = :valor, estado = :estado");
+		$sql->bindValue(':titulo', $titulo);
+		$sql->bindValue(':id_categoria', $categoria);
+		$sql->bindValue(':id_usuario', $_SESSION['cLogin']);
+		$sql->bindValue(':descricao', $descricao);
+		$sql->bindValue(':valor', $valor);
+		$sql->bindValue(':estado', $estado);
+		$sql->execute();
 	}
 }
