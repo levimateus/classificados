5438f0138633e944e485f49d271493f49b25584c
levimateus <levimateus2011@gmail.com>
2018-06-09 17:20:56 -0300

    Aula 5 - Área dos Meus Anúncios

---
+++ classes/anuncios.class.php
@@ -0,0 +1,25 @@
+<?php 
+class Anuncios
+{
+	public function getMeusAnuncios(){
+		global $pdo;
+
+		$array = array();
+		$sql = $pdo->prepare("SELECT *, 
+							   (SELECT anuncios_imagens.url 
+								FROM anuncios_imagens 
+								WHERE anuncios_imagens.id_anuncio = anuncio.id 
+								LIMIT 1) 
+								AS url 
+							FROM anuncios 
+							WHERE id_usuario = :id_usuario");
+		$sql->bindValue(":id_usuario", $_SESSION['cLogin']);
+		$sql->execute();
+
+		if ($sql->rowCount() > 0) {
+			$array = $sql->fetchAll();
+		}
+
+		return $array;
+	}
+}
---
+++ meus-anuncios.php
@@ -0,0 +1,44 @@
+<?php require 'config.php'; ?>
+<?php require 'pages/header.php'; ?>
+<?php 
+	if (empty($_SESSION['cLogin'])) {
+		?>
+		<script type="text/javascript">window.location.href="login.php";</script>
+		<?php
+		exit();
+	}
+ ?>
+
+<div class="container">
+	<h1>Meus Anúncios</h1>
+	<a href="add-anuncio.php" class="btn btn-default">Adicionar Anúncio</a>
+
+	<table class="table table-triped">
+		<thead>
+			<tr>
+				<th>Foto</th>
+				<th>Título</th>
+				<th>Valor</th>
+				<th>Ações</th>
+			</tr>
+		</thead>
+		<?php 
+		require 'classes/anuncios.class.php';
+		$a = new Anuncios();
+		$anuncios = $a->getMeusAnuncios();
+
+		foreach($anuncios as $anuncio):
+		 ?>
+		
+		<tr>
+			<td><img src="assets/images/anuncios/<?php echo $anuncio['url']; ?>" border="0"></td>
+			<td><?php echo $anuncio['title']; ?></td>
+			<td>R$ <?php echo number_format($anuncio['valor'], 2); ?></td>
+			<td></td>
+		</tr>
+
+		<?php endforeach; ?>
+	</table>
+</div>
+
+<?php require 'pages/footer.php'; ?>
