c0c9693a7c5a63869e753f01d17340664a484c69
Mateus Levi <levimateus2011@gmail.com>
2018-07-12 14:12:14 -0300

    AULA EXTRA

--- index.php
+++ index.php
@@ -105,7 +105,11 @@ $categorias = $c->getLista();
 
 				<ul class="pagination">
 					<?php for ($q=1; $q <= $total_paginas; $q++):?>
-					<li class="<?php echo ($p==$q) ? 'active' : ''; ?>"><a href="index.php?p=<?php echo $q; ?>"><?php echo $q ?></a></li>
+					<li class="<?php echo ($p==$q) ? 'active' : ''; ?>"><a href="index.php?p=<?php 
+					$w = $_GET;
+					$w['p'] = $q;
+					echo http_build_query($w);
+					?>"><?php echo $q ?></a></li>
 					<?php endfor; ?>
 				</ul>
 			</div>
