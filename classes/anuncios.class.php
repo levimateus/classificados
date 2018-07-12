38c5844e1ff42b285ab34c1a63fb69cccf8bb144
Mateus Levi <levimateus2011@gmail.com>
2018-07-12 13:50:09 -0300

    Aula 11 - Aplicando filtro às pesquisas

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -1,25 +1,75 @@
 <?php 
 class Anuncios
 {
-	public function getTotalAnuncios(){
+	public function getTotalAnuncios($filtros){
 		global $pdo;
 
-		$sql = $pdo->query("SELECT COUNT(*) AS c FROM anuncios");
+		$filtrostring = array('1=1');
+		if (!empty($filtros['categoria'])) {
+			$filtrostring[] = 'anuncios.id_categoria = :id_categoria';
+		}
+		if (!empty($filtros['preco'])) {
+			$filtrostring[] = 'anuncios.valor BETWEEN :preco1 AND :preco2';
+		}
+		if (!empty($filtros['estado'])) {
+			$filtrostring[] = 'anuncios.estado = :estado';
+		}
+
+		$sql = $pdo->prepare("SELECT COUNT(*) AS c FROM anuncios WHERE ".implode(' AND ', $filtrostring	));
+
+		if (!empty($filtros['categoria'])) {
+			$sql->bindValue(':id_categoria', $filtros['categoria']);
+		}
+		if (!empty($filtros['preco'])) {
+			$preco = explode('-', $filtros['preco']);
+			$sql->bindValue(':preco1', $preco[0]);
+			$sql->bindValue(':preco2', $preco[1]);
+		}
+		if (!empty($filtros['estado'])) {
+			$sql->bindValue(':estado', $filtros['estado']);
+		}
+		
+		$sql->execute();
 		$row = $sql->fetch();
 
 		return $row['c'];
 	}
 
-	public function getUltimosAnuncios($page, $perPage){
+	public function getUltimosAnuncios($page, $perPage, $filtros){
 		global $pdo;
 
 		$offset = ($page - 1) * $perPage;
 
 		$array = array();
+
+		$filtrostring = array('1=1');
+		if (!empty($filtros['categoria'])) {
+			$filtrostring[] = 'anuncios.id_categoria = :id_categoria';
+		}
+		if (!empty($filtros['preco'])) {
+			$filtrostring[] = 'anuncios.valor BETWEEN :preco1 AND :preco2';
+		}
+		if (!empty($filtros['estado'])) {
+			$filtrostring[] = 'anuncios.estado = :estado';
+		}
+
 		$sql = $pdo->prepare("SELECT *, 
 			(SELECT anuncios_imagens.url FROM anuncios_imagens WHERE anuncios_imagens.id_anuncio = anuncios.id LIMIT 1) as url,
 			(SELECT categorias.nome FROM categorias WHERE categorias.id = anuncios.id_categoria) as categoria
-			FROM anuncios ORDER BY id DESC LIMIT $offset, $perPage");
+			FROM anuncios WHERE ".implode(' AND ', $filtrostring)." ORDER BY id DESC LIMIT $offset, $perPage");
+
+		if (!empty($filtros['categoria'])) {
+			$sql->bindValue(':id_categoria', $filtros['categoria']);
+		}
+		if (!empty($filtros['preco'])) {
+			$preco = explode('-', $filtros['preco']);
+			$sql->bindValue(':preco1', $preco[0]);
+			$sql->bindValue(':preco2', $preco[1]);
+		}
+		if (!empty($filtros['estado'])) {
+			$sql->bindValue(':estado', $filtros['estado']);
+		}
+
 		$sql->execute();
 
 		if ($sql->rowCount() > 0) {
--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 <?php 
 require 'classes/anuncios.class.php';
 require 'classes/usuarios.class.php';
+require 'classes/categorias.class.php';
 $a = new Anuncios();
 $u = new Usuarios();
+$c = new Categorias();
 
-$total_anuncios = $a->getTotalAnuncios();
+$filtros = array(
+	'categoria' => '',
+	'preco' => '',
+	'estado' => ''
+);
+if (isset($_GET['filtros'])) {
+	$filtros = $_GET['filtros'];
+}
+
+$total_anuncios = $a->getTotalAnuncios($filtros);
 $total_usuarios = $u->getTotalUsuarios();
 
 $p = 1;
@@ -17,7 +28,8 @@ if (isset($_GET['p']) && !empty($_GET['p'])) {
 $por_pagina = 2;
 $total_paginas = ceil($total_anuncios / $por_pagina);	
 
-$anuncios = $a->getUltimosAnuncios($p, $por_pagina);
+$anuncios = $a->getUltimosAnuncios($p, $por_pagina, $filtros);
+$categorias = $c->getLista();
  ?>
 
 	<div class="container-fluid">
@@ -29,6 +41,42 @@ $anuncios = $a->getUltimosAnuncios($p, $por_pagina);
 		<div class="row">
 			<div class="col-sm-4">
 				<h4>Pesquisa Avançada</h4>
+				<form method="GET">
+					<div class="form-group">
+						<label for="categoria">Categoria:</label>
+						<select id="categoria" name="filtros[categoria]" class="form-control">
+							<option></option>
+							<?php foreach ($categorias as $cat): ?>
+								<option value="<?php echo $cat['id']; ?>" <?php echo ($cat['id'] == $filtros['categoria']) ? 'selected="selected"' : ''; ?>><?php echo utf8_encode($cat['nome']); ?></option>
+							<?php endforeach ?>
+						</select>
+					</div>
+
+					<div class="form-group">
+						<label for="preco">Preço:</label>
+						<select id="preco" name="filtros[preco]" class="form-control">
+							<option></option>
+							<option value="0-50" <?php echo ($filtros['preco'] == '0-50') ? 'selected="selected"' : ''; ?>>R$ 0 - 50</option>
+							<option value="51-100" <?php echo ($filtros['preco'] == '50') ? 'selected="selected"' : ''; ?>>R$ 51 - 100</option>
+							<option value="101-200" <?php echo ($filtros['preco'] == '101-200') ? 'selected="selected"' : ''; ?>>R$ 101 - 200</option>
+							<option value="201-500" <?php echo ($filtros['preco'] == '201-500') ? 'selected="selected"' : ''; ?>>R$ 201 - 500</option>
+						</select>
+					</div>
+
+					<div class="form-group">
+						<label for="estado">Estado:</label>
+						<select id="estado" name="filtros[estado]" class="form-control">
+							<option></option>
+							<option value="0" <?php echo ($filtros['estado'] == '0') ? 'selected="selected"' : ''; ?>>Ruim</option>
+							<option value="1" <?php echo ($filtros['estado'] == '1') ? 'selected="selected"' : ''; ?>>Bom</option>
+							<option value="2" <?php echo ($filtros['estado'] == '2') ? 'selected="selected"' : ''; ?>>Ótimo</option>
+						</select>
+					</div>
+
+					<div class="form-group">
+						<input type="submit" class="btn btn-info" value="Buscar">
+					</div>
+				</form>
 			</div>
 			<div class="col-sm-8">
 				<h4>Últimos Anúncios</h4>
